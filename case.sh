93cd0cd81e3e0d5b56c5d38aa3aed333598d30fd
ankur <itsankur222@outlook.com>
2022-11-09 23:54:57 +0530

    ba

--- case.sh
+++ case.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
 
-echo "Hi user welcome"
-echo  "Please enter your name"
-read name
-echo "welcome $name"
-echo Please select any option from below
-echo 1=date 2=hostname 3=pwd 4=ls 5= to add two numbers
-read choice
+echo hio user welcome to govt portal
+echo please enter your name and age
+read namme
+echo welcome $name now please enter your age
+read age
 
-case $choice in 
-	1) echo $(date);;
-	2) echo $(hostname);;
-	3) pwd;;
-	4)ls;;
-	5) echo please enter the numbers you want to add
-		read num1
-		read num2
-		let sum=$num1+$num2
-		echo sum of numbers is $sum;;
-	*) echo invalid option;;
-esac
-echo this line was written inside github
-echo congratulations your trial was sucessfull
-echo this is the third trial line
-echo this is the third line 
-echo this is the fouth line
+if [ $age -ge 30 ]
+then 
+	echo WELCOME HAVE A DRINK AND ENJOY
+else
+	echo GET THE FUCK OUT OF HERE YOU MAGGOT
+
+fi 
+
+
+
+
